30cb93fac7a24f875dda56655890b441e81d5314
Derek DeVries <derek@maintainable.com>
2008-05-29 18:32:46 +0000

    fix failing mad tests for mysql differences

--- test/Mad/Model/ConnectionAdapter/AbstractTest.php
+++ test/Mad/Model/ConnectionAdapter/AbstractTest.php
@@ -594,9 +594,9 @@ class Mad_Model_ConnectionAdapter_AbstractTest extends Mad_Test_Unit
         "  `name` varchar(255) default NULL,\n".
         "  `is_college` tinyint(1) default NULL,\n".
         "  PRIMARY KEY  (`id`)\n".
-        ") ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;";
+        ") ENGINE=InnoDB";
 
-        $this->assertEquals($expected, $structure);
+        $this->assertContains($expected, $structure);
 
         // entire structure
         $structure = $this->_conn->structureDump();
